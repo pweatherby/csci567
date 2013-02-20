3b7e94e892a1d4ee85072ac1edbb0635e5dce9cb
Paul Weatherby <pweatherby@csuchico.edu>
2013-02-19 18:13:41 -0800

    extra folder level
    /sigh. extra folder level created by last commit. fixing.

--- project_1/Calculator/Calculator/edu_csu_chico_csci_567_paulw_AppDelegate.h
+++ project_1/Calculator/Calculator/edu_csu_chico_csci_567_paulw_AppDelegate.h
@@ -2,8 +2,8 @@
 //  edu_csu_chico_csci_567_paulw_AppDelegate.h
 //  Calculator
 //
-//  Created by Generic Login on 2/19/13.
-//  Copyright (c) 2013 Generic Login. All rights reserved.
+//  Created by Paul Weatherby on 2/19/13.
+//  Copyright (c) 2013 Paul Weatherby. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
