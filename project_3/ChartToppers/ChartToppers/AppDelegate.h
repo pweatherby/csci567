73c056b7f432b268f0b2d157c14a356ff7d336d9
Paul Weatherby <pweatherby@csuchico.edu>
2013-03-28 16:28:39 -0700

    Added Project 3 Default Project

---
+++ project_3/ChartToppers/ChartToppers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChartToppers
+//
+//  Created by Generic Login on 3/28/13.
+//  Copyright (c) 2013 Paul Weatherby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ project_3/ChartToppers/ChartToppers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChartToppers
+//
+//  Created by Generic Login on 3/28/13.
+//  Copyright (c) 2013 Paul Weatherby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
