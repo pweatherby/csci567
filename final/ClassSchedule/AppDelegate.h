f41ad700768237102538b401c3d4aaa04c966e48
pweatherby <pweatherby@csuchico.edu>
2013-04-25 18:58:13 -0700

    Initial Project Creation

---
+++ final/ClassSchedule/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassSchedule
+//
+//  Created by iOS Student on 4/25/13.
+//  Copyright (c) 2013 Paul Weatherby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ final/ClassSchedule/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ClassSchedule
+//
+//  Created by iOS Student on 4/25/13.
+//  Copyright (c) 2013 Paul Weatherby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
